a3bf5290cbe5b48eb53b63ed200a5b57c402503a
Priyansh Shah <shah.priyansh04@gmail.com>
2023-08-28 02:04:44 +0530

    App setup complete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       
+      </div>
+      <p className="read-the-docs text-red-400">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
